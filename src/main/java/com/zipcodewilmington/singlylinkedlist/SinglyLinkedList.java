61e088bd91ff3107e1784f20a1d25e926fca3d81
Michael Krohn <michaelkrohn@MacBook-Pro-8.localdomain>
2019-03-16 13:34:29 -0400

    need to finish sort

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,130 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T extends Comparable>{
+
+    private Node master;
+    private int size;
+    private Node last;
+
+    public SinglyLinkedList() {
+        this.master = new Node(null,null);
+        this.size = 0;
+        this.last = master;
+    }
+
+    public void checkSize(int index){
+        if(index < -1 || index >=size) {
+            throw new IndexOutOfBoundsException("Not valid");
+        }
+    }
+
+    private Node getNode(Integer index){
+
+        checkSize(index -1);
+        Node currentNode = master;
+
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    public T getLastItem(){
+        return last.item;
+    }
+
+    public T getItem(Integer index){
+        return getNode(index).item;
+    }
+
+    public void add(T thing){
+
+        last.next = new Node(thing,null);
+        last = last.next;
+
+        size++;
+    }
+    public void add(T thing, Integer index){
+        getNode(index).next = new Node(thing,getNode(index).next);
+        size++;
+    }
+
+    public T take(Integer index){
+        T dataToTake = getNode(index).item;
+        getNode(index-1).next = getNode(index).next;
+
+        size--;
+
+        return dataToTake;
+    }
+
+    public void addArray(T[] thingArray){
+        for(T thing: thingArray){
+            add(thing);
+        }
+    }
+
+    public boolean contains(T thing){
+        Node currentNode = master;
+
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+            if(currentNode.item.equals(thing)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Integer find(T thing){
+        Node currentNode = master;
+        Integer indexCount = 0;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+            indexCount++;
+            if(currentNode.item.equals(thing)){
+                return indexCount;
+            }
+        }
+
+        return -1;
+    }
+
+    public SinglyLinkedList copy(){
+        return this;
+    }
+
+    public void swapForward(Integer index){
+
+
+        Node node = getNode(index);
+        Node nextNode = node.next;
+        node.next = nextNode.next;
+        nextNode.next = node;
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private class Node{
+        T item;
+        Node next;
+
+        private Node(T item, Node next){
+            this.item = item;
+            this.next = next;
+        }
+
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,117 @@
 package com.zipcodewilmington.singlylinkedlist;
 
-/**
- * Created by leon on 1/10/18.
- */
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SinglyLinkedListTest {
+
+
+
+
+
+    @Test
+    public void add() {
+        SinglyLinkedList<Integer> testList = new SinglyLinkedList<>();
+        Integer expectedItem = 100;
+
+        testList.add(expectedItem);
+        Integer actualItem = testList.getLastItem();
+        Assert.assertEquals(expectedItem,actualItem);
+    }
+
+    @Test
+    public void contains() {
+        SinglyLinkedList<Integer> testList = new SinglyLinkedList<>();
+        Integer expectedItem = 50;
+
+        Assert.assertFalse(testList.contains(50));
+        testList.add(expectedItem);
+
+        Assert.assertTrue(testList.contains(50));
+    }
+
+    @Test
+    public void getNode() {
+        SinglyLinkedList<Integer>  testList = new SinglyLinkedList<Integer>();
+        testList.addArray(new Integer[]{1,2,3,4,5,6,7});
+        Integer expected = 4;
+
+        Integer actual = testList.getItem(4);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getMaster() {
+        SinglyLinkedList<Integer>  testList = new SinglyLinkedList<Integer>();
+        testList.addArray(new Integer[]{1,2,3,4,5,6,7});
+
+        Integer actual = testList.getItem(0);
+
+        Assert.assertNull(actual);
+    }
+
+    @Test
+    public void take() {
+        SinglyLinkedList<Integer>  testList = new SinglyLinkedList<Integer>();
+        testList.addArray(new Integer[]{11,23,37,43,55,63,71});
+        Integer indexToTake = 4;
+        Integer numberToRemove = testList.getItem(indexToTake);
+        Assert.assertTrue(testList.contains(numberToRemove));
+        Integer expectedSize = testList.getSize() -1;
+        Integer preLast = testList.getLastItem();
+
+        Integer takenNumber = testList.take(indexToTake);
+        Integer postSize = testList.getSize();
+        Integer postLast = testList.getLastItem();
+
+
+        Assert.assertFalse(testList.contains(numberToRemove));
+        Assert.assertEquals(expectedSize,postSize);
+        Assert.assertEquals(preLast,postLast);
+        Assert.assertEquals(numberToRemove,takenNumber);
+    }
+
+    @Test
+    public void addIndex() {
+        SinglyLinkedList<Integer>  testList = new SinglyLinkedList<Integer>();
+        testList.addArray(new Integer[]{1,2,3,4,5,6,7});
+        Integer intToAdd = 90;
+        Integer indexToAdd = 5;
+
+        testList.add(intToAdd,indexToAdd);
+        Integer actual = testList.getItem(indexToAdd);
+
+        Assert.assertEquals(indexToAdd,actual);
+    }
+
+    @Test
+    public void find(){
+        SinglyLinkedList<Integer> testList = new SinglyLinkedList<>();
+        Integer expectedItem = 58;
+
+        Assert.assertFalse(testList.contains(58));
+        testList.add(expectedItem);
+
+        Integer expectedIndex = 1;
+
+        Assert.assertEquals(expectedIndex,testList.find(58));
+    }
+
+    @Test
+    public void swapForward(){
+        SinglyLinkedList<Integer>  testList = new SinglyLinkedList<Integer>();
+        testList.addArray(new Integer[]{1,2,55,46,5,6,7});
+        Integer index = 4;
+        Integer expected = testList.getItem(4);
+
+
+        testList.swapForward(3);
+
+        Assert.assertEquals(expected,testList.getItem(3));
+    }
+
+
 }
